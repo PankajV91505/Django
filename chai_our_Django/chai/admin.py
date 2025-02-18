b8e831579ac01884d9cad7569145fb715a30084b
Pankaj Verma <pankajv91505@gmail.com>
2025-02-19 00:01:31 +0530

    update the code

--- chai_our_Django/chai/admin.py
+++ chai_our_Django/chai/admin.py
@@ -1,5 +1,23 @@
 from django.contrib import admin
-from .models import ChaiVariety
+from .models import ChaiVariety , ChaiReview , Store , ChaiCertification
 
 # Register your models here.
-admin.site.register(ChaiVariety)
+class ChaiReviewInline(admin.TabularInline):
+    model = ChaiReview
+    extra = 2
+    
+class ChaiVarietyAdmin(admin.ModelAdmin):
+    list_display = ('name', 'type' , 'date_added')
+    inlines = [ChaiReviewInline]
+    
+class StoreAdmin(admin.ModelAdmin):
+    list_display = ('name', 'location')
+    filter_horizontal = ('chai_varieties',)
+    
+class ChaiCertificationAdmin(admin.ModelAdmin):
+    list_display = ('chai', 'certification_number', 'date_issued', 'valid_till')
+    
+admin.site.register(ChaiVariety , ChaiVarietyAdmin)
+# admin.site.register(ChaiReview)
+admin.site.register(Store , StoreAdmin)
+admin.site.register(ChaiCertification , ChaiCertificationAdmin)
--- chai_our_Django/chai/models.py
+++ chai_our_Django/chai/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 from django.utils import timezone
+from django.contrib.auth.models import User
 
 # Create your models here.
 
@@ -20,4 +21,41 @@ class ChaiVariety(models.Model):
     def __str__(self):
         return self.name
     
+
+
+# one to many relationship
+
+class ChaiReview(models.Model):
+    chai = models.ForeignKey(ChaiVariety, on_delete=models.CASCADE , related_name='reviews')
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    date_added = models.DateTimeField(default=timezone.now)
+    rating = models.IntegerField()
+    review = models.TextField(default = '')
+    comment = models.TextField(default = '')
+    def __str__(self):
+        return f'{self.chai.name} review by {self.user.username}'
     
+    
+    
+
+
+# many to many relationship
+
+class Store(models.Model):
+    name = models.CharField(max_length=100)
+    location = models.CharField(max_length=100)
+    chai_varieties = models.ManyToManyField(ChaiVariety , related_name='stores')
+    
+    def __str__(self):
+        return self.name
+    
+    
+# one to one relationship
+
+class ChaiCertification(models.Model):
+    chai = models.OneToOneField(ChaiVariety, on_delete=models.CASCADE)
+    certification_number = models.CharField(max_length=100)
+    date_issued = models.DateTimeField(default=timezone.now)
+    valid_till = models.DateTimeField()
+    def __str__(self):
+        return f'CERTIFICATION FOR {self.name.chai}'
