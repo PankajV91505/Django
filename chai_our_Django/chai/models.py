23e8431906b81e6ff382a9f4153b3aedba2741d6
Pankaj Verma <pankajv91505@gmail.com>
2025-02-18 11:04:20 +0530

    add more code in the project

--- chai_our_Django/chai/models.py
+++ chai_our_Django/chai/models.py
@@ -1,3 +1,23 @@
 from django.db import models
+from django.utils import timezone
 
 # Create your models here.
+
+class ChaiVariety(models.Model):
+    CHAI_TYPE_CHOICES = [
+        ('ML' , 'MASALA'),
+        ('KL' , 'KADAK'),
+        ('IL' , 'IRANI'),
+        ('PL' , 'PLAIN'),
+        ('SL' , 'SUGARLESS'),
+    ]
+    name = models.CharField(max_length=100)
+    description = models.TextField(default = '')
+    price = models.DecimalField(max_digits=5, decimal_places=2)
+    image = models.ImageField(upload_to='chai/images/')
+    date_added = models.DateTimeField(default=timezone.now)
+    type = models.CharField(max_length=2, choices=CHAI_TYPE_CHOICES )
+    def __str__(self):
+        return self.name
+    
+    
--- chai_our_Django/chai/views.py
+++ chai_our_Django/chai/views.py
@@ -1,5 +1,13 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from .models import ChaiVariety
+from django.shortcuts import get_object_or_404
+
 
 def all_chai(request):
-    return render(request, 'all_chai/index.html') 
+    chais = ChaiVariety.objects.all()
+    return render(request, 'chai/all_chai.html', {'chais': chais}) 
+
+def chai_detail(request, chai_id):
+    chai = get_object_or_404(ChaiVariety, pk=chai_id)
+    return render(request, 'chai/chai_detail.html', {'chai': chai})
