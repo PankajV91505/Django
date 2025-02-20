eeb406c3fe6842612df7beb1be264b153ee7ee56
Pankaj Verma <pankajv91505@gmail.com>
2025-02-20 19:39:24 +0530

    update file

--- chai_our_Django/chai/urls.py
+++ chai_our_Django/chai/urls.py
@@ -7,6 +7,8 @@ from . import views
 urlpatterns = [
     path('', views.all_chai, name='all_chai'),
     path('<int:chai_id>/', views.chai_detail, name='chai_detail'),
+    path('chai_store/', views.chai_store, name='chai_store'),
+    
     
     
 ]
--- chai_our_Django/chai/views.py
+++ chai_our_Django/chai/views.py
@@ -1,7 +1,9 @@
 from django.shortcuts import render
 from django.http import HttpResponse
-from .models import ChaiVariety
+from .models import ChaiVariety , Store
 from django.shortcuts import get_object_or_404
+from .forms import ChaiVarietyForm
+
 
 
 def all_chai(request):
@@ -11,3 +13,14 @@ def all_chai(request):
 def chai_detail(request, chai_id):
     chai = get_object_or_404(ChaiVariety, pk=chai_id)
     return render(request, 'chai/chai_detail.html', {'chai': chai})
+
+def chai_store(request):
+    stores = None 
+    if request.method == 'POST':
+        form = ChaiVarietyForm(request.POST)
+        if form.is_valid():
+            chai_varity = form.cleaned_data['chai_variety']
+            stores = Store.objects.filter(chai_varieties=chai_variety)  
+    else:
+        form = ChaiVarietyForm()
+    return render(request, 'chai/chai_store.html', {'stores': stores} , {'form': form})
